b3d0e0889c0627743abfb3feab8a37edd9597770
Richard Oyudo <ebube.rc@gmail.com>
2018-05-29 16:32:48 +0100

    Change image and color scheme

--- src/container/Details/index.js
+++ src/container/Details/index.js
@@ -16,9 +16,9 @@ class Details extends React.Component{
                     <Link to="portfolio" spy={true} smooth={true} duration={500}> Portfolio </Link>//
                     <Link to="contact" spy={true} smooth={true} duration={500}> Contact </Link>
                 </span>
-                <About pageHeight={this.props.pageHeight} colours={{'bg':'#fff','fg':'#000','accent':'#999'}}/>
-                <Portfolio pageHeight={this.props.pageHeight} colours={{'bg':'#999','fg':'#fff','accent':'#000'}}/>
-                <Contact pageHeight={this.props.pageHeight} colours={{'bg':'#fff','fg':'#000','accent':'#999'}}/>
+                <About pageHeight={this.props.pageHeight} colours={{'bg':'#fff5cc','fg':'#000','accent':'#999'}}/>
+                <Portfolio pageHeight={this.props.pageHeight} colours={{'bg':'#9ebcc1','fg':'#fff','accent':'#4f4e4f'}}/>
+                <Contact pageHeight={this.props.pageHeight} colours={{'bg':'#c4b2c3','fg':'#4f4e4f','accent':'#fff'}}/>
                 <Footer pageHeight='100px' colours={{'bg':'#999','fg':'#fff','accent':'#000'}}/>
             </div>
         )
