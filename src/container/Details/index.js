9ce77caab3bca3aca6f8ed0cc11577108821a40c
Richard Oyudo <ebube.rc@gmail.com>
2018-05-29 16:48:03 +0100

    Simplify color scheme

--- src/container/Details/index.js
+++ src/container/Details/index.js
@@ -17,9 +17,9 @@ class Details extends React.Component{
                     <Link to="contact" spy={true} smooth={true} duration={500}> Contact </Link>
                 </span>
                 <About pageHeight={this.props.pageHeight} colours={{'bg':'#fff5cc','fg':'#000','accent':'#999'}}/>
-                <Portfolio pageHeight={this.props.pageHeight} colours={{'bg':'#9ebcc1','fg':'#fff','accent':'#4f4e4f'}}/>
-                <Contact pageHeight={this.props.pageHeight} colours={{'bg':'#c4b2c3','fg':'#4f4e4f','accent':'#fff'}}/>
-                <Footer pageHeight='100px' colours={{'bg':'#999','fg':'#fff','accent':'#000'}}/>
+                <Portfolio pageHeight={this.props.pageHeight} colours={{'bg':'#9ebcc1','fg':'#fff','accent':'#000'}}/>
+                <Contact pageHeight={this.props.pageHeight} colours={{'bg':'#fff5cc','fg':'#000','accent':'#999'}}/>
+                <Footer pageHeight='100px' colours={{'bg':'#9ebcc1','fg':'#fff','accent':'#000'}}/>
             </div>
         )
     }
